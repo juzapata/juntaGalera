dd4998fcf1ae2e333a6a250d7c90e5b4501cf222
solangealmn <solangealmn@gmail.com>
2018-08-13 16:55:01 -0300

    Iniciando projeto.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBxWV1dIbnDKOZx84rI0zi2gR8T8qt8yMY",
+  authDomain: "juntagalera-rs18.firebaseapp.com",
+  databaseURL: "https://juntagalera-rs18.firebaseio.com",
+  projectId: "juntagalera-rs18",
+  storageBucket: "juntagalera-rs18.appspot.com",
+  messagingSenderId: "442263235658"
+};
+firebase.initializeApp(config);
