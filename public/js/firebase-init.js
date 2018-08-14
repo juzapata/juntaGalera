dda9c820f951c0b7351b7084c7e48edb4910504a
solangealmn <solangealmn@gmail.com>
2018-08-14 17:34:59 -0300

    Separação dos arquivos.

---
+++ public/js/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBQgxA0tsGpb-2PMSZnRX-bQIVJSsRenYE",
+  authDomain: "juntagalera1.firebaseapp.com",
+  databaseURL: "https://juntagalera1.firebaseio.com",
+  projectId: "juntagalera1",
+  storageBucket: "juntagalera1.appspot.com",
+  messagingSenderId: "874770036714"
+};
+firebase.initializeApp(config);
